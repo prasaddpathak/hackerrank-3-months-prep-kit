1c7e2b4893e994edc83e8598e28939d73e08a674
Prasad Pathak <prasaddpathak@gmail.com>
2022-08-05 13:35:34 -0400

    Solution for counting valley

--- week2/CountingSorts1.java
+++ week2/CountingSorts1.java
@@ -1,2 +1,66 @@
-package week2;public class CountingSorts1 {
+package week2;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        Integer[] arrCount = new Integer[100];
+        for (int i = 0; i < 100; i++) {
+            arrCount[i] = 0;
+        }
+        for(int i = 0 ; i < arr.size() ; i++){
+            arrCount[arr.get(i)] = ++arrCount[arr.get(i)];
+        }
+
+        return Arrays.asList(arrCount);
+    }
 }
+
+public class CountingSorts1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.countingSort(arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
